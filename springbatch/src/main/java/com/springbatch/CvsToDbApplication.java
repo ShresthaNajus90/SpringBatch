53ba960a3108d2ad2d2a1f03450cdc7b1510143c
ShresthaNajus90 <151704957+ShresthaNajus90@users.noreply.github.com>
2023-12-29 15:45:16 -0700

    Add files via upload
    Spring batch done.

---
+++ springbatch/src/main/java/com/springbatch/CvsToDbApplication.java
@@ -0,0 +1,13 @@
+package com.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvsToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvsToDbApplication.class, args);
+	}
+
+}
